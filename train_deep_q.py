931ab8f9b07b08a7cdde39e87855e06859e9c3b0
Omar Cobas <omarlcobas@gmail.com>
2023-05-24 12:04:51 -0400

    progress on deep Q

--- train_deep_q.py
+++ train_deep_q.py
@@ -1,5 +1,10 @@
 import random
+import torch.optim as optim
+import string
 from nltk.metrics.distance import edit_distance
+from transformers import T5Tokenizer, T5ForConditionalGeneration
+import numpy as np
+import torch
 
 class ASRErrorCorrectionEnvironment:
     def __init__(self, clean_sentence, error_prob):
@@ -7,10 +12,41 @@ class ASRErrorCorrectionEnvironment:
         self.error_prob = error_prob
         self.current_sentence, self.error_position = self.generate_synthetic_errors(clean_sentence, error_prob)
         self.done = False
+        # Define the action space
+        self.action_space = self.generate_action_space(clean_sentence)
+
+    def generate_action_space(self, sentence):
+        action_space = []
+
+        # Define the types of actions
+        action_types = ["insertion", "deletion", "substitution", "no_change"]
+
+        # Define the positions
+        positions = range(len(sentence))
+
+        # Define the characters
+        characters = list(string.ascii_lowercase)  # Example: lowercase letters
+
+        # Generate all possible combinations of actions
+        for action_type in action_types:
+            for position in positions:
+                if action_type == "no_change" and position != self.error_position:
+                    continue  # Skip "no_change" action at positions other than the error position
+                for char in characters:
+                    action = {
+                        "type": action_type,
+                        "position": position,
+                        "char": char
+                    }
+                    action_space.append(action)
+
+        return action_space
+
 
     def reset(self):
         self.current_sentence, self.error_position = self.generate_synthetic_errors(self.clean_sentence, self.error_prob)
         self.done = False
+        return self.current_sentence, self.error_position
 
     def generate_synthetic_errors(self, sentence, error_prob):
           sentence_chars = list(sentence)
@@ -37,6 +73,9 @@ class ASRErrorCorrectionEnvironment:
           return "".join(sentence_chars), error_position
 
     def step(self, action):
+        # Unpack the action components
+        action_type, action_char, action_position = action
+
         # Apply the chosen action to the current sentence
         # Update the error position based on the action
 
@@ -44,15 +83,15 @@ class ASRErrorCorrectionEnvironment:
         previous_distance = edit_distance(self.current_sentence, self.clean_sentence)
 
         # Update the current sentence based on the action
-        if action["type"] == "insertion":
+        if action_type == "insertion":
             # Apply insertion action
-            self.current_sentence = self.current_sentence[:action["position"]] + action["char"] + self.current_sentence[action["position"]:]
-        elif action["type"] == "deletion":
+            self.current_sentence = self.current_sentence[:action_position] + action_char + self.current_sentence[action_position:]
+        elif action_type == "deletion":
             # Apply deletion action
-            self.current_sentence = self.current_sentence[:action["position"]] + self.current_sentence[action["position"] + 1:]
-        elif action["type"] == "substitution":
+            self.current_sentence = self.current_sentence[:action_position] + self.current_sentence[action_position + 1:]
+        elif action_type == "substitution":
             # Apply substitution action
-            self.current_sentence = self.current_sentence[:action["position"]] + action["char"] + self.current_sentence[action["position"] + 1:]
+            self.current_sentence = self.current_sentence[:action_position] + action_char + self.current_sentence[action_position + 1:]
 
         # Calculate the new edit distance
         current_distance = edit_distance(self.current_sentence, self.clean_sentence)
@@ -66,104 +105,109 @@ class ASRErrorCorrectionEnvironment:
         # Return the updated state, reward, and done flag
         return self.current_sentence, reward, self.done
 
+
     def get_state(self):
         # Return the current state
         return self.current_sentence, self.error_position
 
-import random
-import torch
-import torch.optim as optim
-import torch.nn.functional as F
-from torch.utils.data import DataLoader
-from transformers import T5Tokenizer, T5ForConditionalGeneration
-from collections import deque
-
-# Define hyperparameters
-num_episodes = 1000
-max_steps_per_episode = 100
-replay_buffer_size = 10000
-batch_size = 32
-learning_rate = 0.001
-gamma = 0.99
-epsilon_start = 1.0
-epsilon_end = 0.01
-epsilon_decay = 0.99
-
-# Initialize the environment and RL agent
-env = ASRErrorCorrectionEnvironment(clean_sentence, error_prob)
-tokenizer = T5Tokenizer.from_pretrained("t5-base")
-model = T5ForConditionalGeneration.from_pretrained("t5-base")
-target_model = T5ForConditionalGeneration.from_pretrained("t5-base")
-target_model.load_state_dict(model.state_dict())
-optimizer = optim.Adam(model.parameters(), lr=learning_rate)
-replay_buffer = deque(maxlen=replay_buffer_size)
-
 # Epsilon-greedy exploration strategy
-def epsilon_greedy_action(state, epsilon):
+def epsilon_greedy_action(q_values, epsilon, num_chars, action_mapping):
+    # Generate random value for exploration
     if random.random() < epsilon:
-        return random.randint(0, len(env.action_space) - 1)
+        # Randomly select an action
+        action_type = random.choice(["insertion", "deletion", "substitution", "no_change"])
+        action_char = random.choice(string.ascii_lowercase)  # Choose a random lowercase character
+        action_position = random.randint(0, num_chars - 1)  # Choose a random position
+
+        return action_type, action_char, action_position
     else:
-        q_values = model(state)
-        return torch.argmax(q_values).item()
+        # Select the action with the highest Q-value
+        action_index = np.argmax(q_values)
+        action_type, action_char, action_position = action_mapping[action_index]
 
-# Main training loop
+        return action_type, action_char, action_position
+
+
+# Initialize the T5 model and tokenizer
+model = T5ForConditionalGeneration.from_pretrained('t5-base')
+tokenizer = T5Tokenizer.from_pretrained('t5-base')
+
+action_mapping = [
+    ("insertion", None, None),  # Action index 0 corresponds to insertion
+    ("deletion", None, None),   # Action index 1 corresponds to deletion
+    ("substitution", None, None),  # Action index 2 corresponds to substitution
+    ("no_change", None, None),   # Action index 3 corresponds to no_change
+]
+
+# Define the epsilon value for exploration
+epsilon = 0.2
+discount_factor = 0.9
+
+# Define the learning rate and other optimizer parameters
+learning_rate = 0.001
+weight_decay = 0.0001
+
+# Define the optimizer
+optimizer = optim.Adam(model.parameters(), lr=learning_rate, weight_decay=weight_decay)
+
+
+num_episodes = 1000
+clean_sentence = "the quick brown fox jumps over the lazy dog"
+error_prob = 0.5
+env = ASRErrorCorrectionEnvironment(clean_sentence, error_prob)
+
+# Training loop
 for episode in range(num_episodes):
+    # Reset the environment
     state = env.reset()
-    episode_reward = 0
-    epsilon = max(epsilon_end, epsilon_start * epsilon_decay ** episode)
+    done = False
 
-    for step in range(max_steps_per_episode):
-        # Select an action
-        epsilon = max(epsilon_end, epsilon_start * epsilon_decay ** episode)
-        action = epsilon_greedy_action(state, epsilon)
+    while not done:
+        # Get the current state
+        current_sentence, error_position = state
 
-        # Take the action and observe the next state, reward, and done flag
-        next_state, reward, done = env.step(action)
-        episode_reward += reward
+        # Tokenize the current sentence
+        inputs = tokenizer.encode_plus(current_sentence, return_tensors='pt', padding='max_length', truncation=True, max_length=512)
 
-        # Store the transition in the replay buffer
-        replay_buffer.append((state, action, reward, next_state, done))
-        state = next_state
+        # Forward pass through the T5 model
+        outputs = model(**inputs)
 
-        # Perform an update if enough samples are available in the replay buffer
-        if len(replay_buffer) >= batch_size:
-            batch = random.sample(replay_buffer, batch_size)
-            states, actions, rewards, next_states, dones = zip(*batch)
+        # Extract the logits
+        logits = outputs.logits.squeeze(0)
 
-            # Convert the batch to tensors
-            states = torch.tensor(states)
-            actions = torch.tensor(actions)
-            rewards = torch.tensor(rewards)
-            next_states = torch.tensor(next_states)
-            dones = torch.tensor(dones)
+        # Convert logits to Q-values
+        q_values = logits.detach().numpy()
 
-            # Compute the Q-values for the current and next states
-            q_values = model(states)
-            next_q_values = target_model(next_states)
+        # Choose the action based on epsilon-greedy policy
+        action_type, action_char, action_position = epsilon_greedy_action(q_values, epsilon, len(current_sentence))
 
-            # Compute the target Q-values using the Bellman equation
-            target_q_values = rewards + (1 - dones) * gamma * torch.max(next_q_values, dim=1).values
+        # Perform the action in the environment
+        next_sentence, reward, done = env.step({"type": action_type, "char": action_char, "position": action_position})
 
-            # Compute the Q-values for the taken actions
-            q_values = q_values.gather(1, actions.unsqueeze(1)).squeeze()
+        # Update the Q-value of the chosen action
+        next_inputs = tokenizer.encode_plus(next_sentence, return_tensors='pt', padding='max_length', truncation=True, max_length=512)
+        next_outputs = model(**next_inputs)
+        next_logits = next_outputs.logits.squeeze(0)
+        next_q_values = next_logits.detach().numpy()
+        max_q_value = np.max(next_q_values)
+        target_q_value = reward + discount_factor * max_q_value
+        q_values[action_mapping.index((action_type, action_char, action_position))] = target_q_value
 
-            # Compute the loss
-            loss = F.smooth_l1_loss(q_values, target_q_values)
+        # Convert Q-values to logits
+        updated_logits = torch.tensor(q_values).unsqueeze(0)
 
-            # Perform the optimization step
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
+        # Backward pass through the T5 model
+        updated_outputs = model(**inputs, labels=updated_logits)
 
-        if done:
-            break
+        # Update the model with the updated logits
+        updated_outputs.loss.backward()
+        optimizer.step()
 
-    # Update the target network every few episodes
-    if episode % target_update_interval == 0:
-        target_model.load_state_dict(model.state_dict())
+        # Clear gradients
+        optimizer.zero_grad()
 
-    # Print the episode reward
-    print(f"Episode {episode+1}: Reward = {episode_reward}")
+        # Update the state for the next iteration
+        state = next_sentence
 
-# Save the trained model
-torch.save(model.state_dict(), "trained_model.pth")
+    # Print the episode number and final Q-values for monitoring
+    print(f"Episode: {episode + 1}, Final Q-values: {q_values}")
