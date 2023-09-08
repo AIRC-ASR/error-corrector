aacb98074a53b513518eeeff0d3c1314c107e7d3
Omar Cobas <omarlcobas@gmail.com>
2023-09-08 15:45:21 -0400

    a

--- phonetic_clustering.py
+++ phonetic_clustering.py
@@ -4,25 +4,9 @@ from nltk import edit_distance
 import matplotlib.pyplot as plt
 import pandas as pd
 import csv
-import Levenshtein
-from scipy.cluster.hierarchy import linkage, fcluster
-from scipy.spatial.distance import pdist
-
-def cluster_strings(strings):
-    # Preprocess strings
-    preprocessed_strings = strings
-    
-    # Calculate pairwise distance matrix
-    distance_matrix = pdist(preprocessed_strings, metric='levenshtein')
-    
-    # Perform hierarchical agglomerative clustering
-    linkage_matrix = linkage(distance_matrix, method='complete')
-    
-    # Determine clusters based on a distance threshold
-    threshold = 0.5  # Adjust this threshold based on your data
-    clusters = fcluster(linkage_matrix, threshold, criterion='distance')
-    
-    return clusters
+import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.cluster import KMeans
 
 def calculateWER(reference, hypothesis):
   reference_words = reference.split()
@@ -75,5 +59,45 @@ if __name__ == '__main__':
 
   # Select the sentence and label columns only
   X = df.loc[:, ['sentence', 'label']].values
+  X_original = df.loc[:, ['sentence', 'label']].values
 
-  cluster_strings(X)
+  vectorizer = TfidfVectorizer(stop_words='english')
+  X = vectorizer.fit_transform(sentences)
+
+  wcss = []
+  for i in range(1,11): 
+    kmeans = KMeans(n_clusters=i, init ='k-means++', max_iter=300,  n_init=10,random_state=0 )
+
+    kmeans.fit(X)
+    wcss.append(kmeans.inertia_)
+
+  plt.plot(range(1,11),wcss)
+  plt.title('The Elbow Method Graph')
+  plt.xlabel('Number of clusters')
+  plt.ylabel('WCSS')
+  plt.savefig('elbow_method.png')
+
+  kmeans = KMeans(n_clusters=7, init ='k-means++', max_iter=300, n_init=10,random_state=0 )
+  y_kmeans = kmeans.fit_predict(X)
+
+  # plt.scatter(X_original[y_kmeans==0, 0], X_original[y_kmeans==0, 1], s=100, c='red', label ='Cluster 1')
+  # plt.scatter(X_original[y_kmeans==1, 0], X_original[y_kmeans==1, 1], s=100, c='blue', label ='Cluster 2')
+  # plt.scatter(X_original[y_kmeans==2, 0], X_original[y_kmeans==2, 1], s=100, c='green', label ='Cluster 3')
+  # plt.scatter(X_original[y_kmeans==3, 0], X_original[y_kmeans==3, 1], s=100, c='cyan', label ='Cluster 4')
+  # plt.scatter(X_original[y_kmeans==4, 0], X_original[y_kmeans==4, 1], s=100, c='magenta', label ='Cluster 5')
+  # plt.scatter(X_original[y_kmeans==5, 0], X_original[y_kmeans==5, 1], s=100, c='darkorange', label ='Cluster 6')
+  # plt.scatter(X_original[y_kmeans==6, 0], X_original[y_kmeans==6, 1], s=100, c='darkviolet', label ='Cluster 7')
+
+  # plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], s=300, c='yellow', label = 'Centroids')
+  # plt.title('Clusters of Customers')
+  # plt.xlabel('Annual Income(k$)')
+  # plt.ylabel('Spending Score(1-100')
+  # plt.savefig('clusters.png')
+
+  order_centroids = kmeans.cluster_centers_.argsort()[:, ::-1]
+  terms = vectorizer.get_feature_names_out()
+  for i in range(7):
+      print("Cluster %d:" % i),
+      for ind in order_centroids[i, :10]:
+          print(' %s' % terms[ind]),
+      print
